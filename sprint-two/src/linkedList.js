a571cef63bd752b18df700c5a50784a105eae36c
riahk <moar.riah@gmail.com>
2015-03-26 15:34:55 -0700

    Implement LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,44 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    //create a new node
+    var node = Node(value);
+    //set tail.next = new node
+    if(list.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    //set list.tail = new node
   };
 
   list.removeHead = function(){
+    var oldHead = this.head;
+
+    if(this.head === this.tail) {
+      list.head = null;
+      list.tail = null;
+
+    } else {
+      this.head = this.head.next;
+      oldHead.next = null;
+    }
+
+    return oldHead.value;
   };
 
   list.contains = function(target){
+    var node = this.head;
+    while(node !== null) {
+      if(node.value === target) {
+        return true;
+      }
+      node = node.next;
+    }
+
+    return false;
   };
 
   return list;
