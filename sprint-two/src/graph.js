3bffde6311c8550c16c730e023f65993c5d14746
riahk <moar.riah@gmail.com>
2015-03-26 16:41:18 -0700

    'working on graph'

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,15 +1,31 @@
 
 
 var Graph = function(){
+  var graph = Object.create(Graph.prototype);
+  graph.data = [];
+  return graph;
 };
 
 Graph.prototype.addNode = function(node){
+  var nodeObj = { value: node, edges: [] };
+  this.data.push(nodeObj);
 };
 
 Graph.prototype.contains = function(node){
+  for(var i = 0; i < this.data.length; i++) {
+    if(this.data[i].value === node) {
+      return true;
+    }
+  }
+  return false;
 };
 
 Graph.prototype.removeNode = function(node){
+  for(var i = 0; i < this.data.length; i++) {
+    if(this.data[i].value === node) {
+      for(var j = )
+    }
+  }
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
