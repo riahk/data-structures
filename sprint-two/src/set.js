1737dc4c124fc1f6ac4c22c951c2529f57671ce4
riahk <moar.riah@gmail.com>
2015-03-27 12:21:10 -0700

    'set done'

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,23 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if(!this.contains(item)){
+      this._storage[item]=item;
+  }
 };
 
 setPrototype.contains = function(item){
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item){
+  delete this._storage[item];
 };
 
 /*
